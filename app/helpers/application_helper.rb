3b0a6607ada0212794984cdb70a1076c2bbc2867
Ubuntu <ubuntu@ip-172-31-3-245.us-east-2.compute.internal>
2021-05-03 03:31:00 +0000

    sublayout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページタイトル
+  def full_title(page_title = '')
+    base_title = "とま〜るウェブ"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
